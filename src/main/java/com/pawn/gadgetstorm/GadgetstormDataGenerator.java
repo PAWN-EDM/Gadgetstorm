f7699dfbb6c3d5f130dcdfc582885b38a8797144
pawn_edm <aidandalzell833@gmail.com>
2024-08-05 10:23:26 -0700

    fixed datagen copy error

--- src/main/java/com/pawn/gadgetstorm/GadgetstormDataGenerator.java
+++ src/main/java/com/pawn/gadgetstorm/GadgetstormDataGenerator.java
@@ -2,10 +2,17 @@ package com.pawn.gadgetstorm;
 
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+import com.pawn.gadgetstorm.datagen.*;
 
 public class GadgetstormDataGenerator implements DataGeneratorEntrypoint {
 	@Override
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+		FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
 
+		pack.addProvider(ModBlockTagProvider::new);
+		pack.addProvider(ModItemTagProvider::new);
+		pack.addProvider(ModLootTableProvider::new);
+		pack.addProvider(ModModelProvider::new);
+		pack.addProvider(ModRecipeProvider::new);
 	}
 }
